2e04d009214a3d4d6ddf3acbd0b085fbef18847c
Gavin OMelia <gavin@omelia.org>
2015-10-06 06:49:28 -0400

    Work on simple interest

--- lib/12_simple_interest/c.ex
+++ lib/12_simple_interest/c.ex
@@ -5,28 +5,17 @@ defmodule SimpleInterest.C do
     number |> Float.to_string [decimals: 2]
   end
 
-  def calculate_simple_interest(principal, interest, years) do
-    (1 + ((interest / 100) * years)) * principal
+  def calculate_simple_interest(principal, interest, year) do
+    (1 + ((interest / 100) * year)) * principal
   end
 
-  def print_multiple_times(msg, n) when n <= 1 do
-    IO.puts msg
-    IO.puts n
+  def go do
+    principal = retrieve("Enter the principal: ")
+    interest = retrieve("Enter the rate of interest: ")
+    years = retrieve("Enter the number of years: ")
+    year_range = Range.new(1, years |> trunc)
+    year = Enum.reduce(year_range, fn(num, acc) -> num end)
+
+    IO.puts "\nAfter #{years} years at #{interest}%, the investment will be worth $#{calculate_simple_interest(principal, interest, year) |> compact}."
   end
-
-  def print_multiple_times(msg, n) do
-    IO.puts msg
-    IO.puts n
-    print_multiple_times(msg, n - 1)
-  end
-
-def go do
-  principal = retrieve("Enter the principal: ")
-  interest = retrieve("Enter the rate of interest: ")
-  years = retrieve("Enter the number of years: ")
-
-  print_multiple_times("\nAfter #{years} years at #{interest}%, the investment will be worth $#{calculate_simple_interest(principal, interest, years) |> compact}." , years)
-
-  IO.puts "\nAfter #{years} years at #{interest}%, the investment will be worth $#{calculate_simple_interest(principal, interest, years) |> compact}."
-end
 end
