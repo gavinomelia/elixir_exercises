b81d3712a50f589afcf8371cd3e267a159a4b05a
Gavin OMelia <gavin@omelia.org>
2015-10-07 06:18:16 -0400

    Change Enum.reduce to Enum.each

--- lib/12_simple_interest/d.ex
+++ lib/12_simple_interest/d.ex
@@ -1,4 +1,4 @@
-defmodule SimpleInterest.C do
+defmodule SimpleInterest.D do
 
   import InputRetriever
   def compact(number) do
@@ -13,7 +13,7 @@ defmodule SimpleInterest.C do
     principal = retrieve("Enter the principal: ")
     interest = retrieve("Enter the rate of interest: ")
     years = retrieve("Enter the number of years: ") |> trunc
-    Enum.each(1..years, fn(year) -> (
+    Enum.reduce(1..years, 0, fn(year, acc) -> (
       IO.puts "\nAfter #{year} year/s at #{interest}%, the investment will be worth $#{calculate_simple_interest(principal, interest, year) |> compact}.")
     end)
   end
