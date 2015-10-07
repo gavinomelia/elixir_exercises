e154efa0d49022ac5d3b87fc32d0e774bd372c44
Gavin OMelia <gavin@omelia.org>
2015-10-07 08:12:44 -0400

    Simple interest d.ex

--- lib/12_simple_interest/d.ex
+++ lib/12_simple_interest/d.ex
@@ -13,7 +13,7 @@ defmodule SimpleInterest.D do
     principal = retrieve("Enter the principal: ")
     interest = retrieve("Enter the rate of interest: ")
     years = retrieve("Enter the number of years: ") |> trunc
-    Enum.reduce(1..years, 0, fn(year, acc) -> (
+    Enum.each(1..years, fn(year) -> (
       IO.puts "\nAfter #{year} year/s at #{interest}%, the investment will be worth $#{calculate_simple_interest(principal, interest, year) |> compact}.")
     end)
   end
