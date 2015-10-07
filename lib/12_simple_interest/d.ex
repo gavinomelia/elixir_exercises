a806a173bd6036794b8b922d7a2b0ba2c01de929
Gavin OMelia <gavin@omelia.org>
2015-10-07 08:14:31 -0400

    Fix the fact that I accidentaly put d.ex in c.ex

--- lib/12_simple_interest/d.ex
+++ lib/12_simple_interest/d.ex
@@ -5,16 +5,18 @@ defmodule SimpleInterest.D do
     number |> Float.to_string [decimals: 2]
   end
 
-  def calculate_simple_interest(principal, interest, year) do
-    (1 + ((interest / 100) * year)) * principal
-  end
-
   def go do
     principal = retrieve("Enter the principal: ")
     interest = retrieve("Enter the rate of interest: ")
     years = retrieve("Enter the number of years: ") |> trunc
-    Enum.each(1..years, fn(year) -> (
-      IO.puts "\nAfter #{year} year/s at #{interest}%, the investment will be worth $#{calculate_simple_interest(principal, interest, year) |> compact}.")
+    increase_per_year = (interest / 100) * principal
+    { yearly_totals, _ } = Enum.map_reduce(1..years, principal, fn(year, acc) ->
+      yearly_total = increase_per_year + acc
+      {{year, yearly_total}, yearly_total}
+    end)
+
+    Enum.each(yearly_totals, fn({ year, year_total }) ->
+      IO.puts "\nAfter #{year} year/s at #{interest}%, the investment will be worth $#{year_total |> compact}"
     end)
   end
 end
