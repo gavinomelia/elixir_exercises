e0e6a52fec7c1d5f1a08d592fc337c0a2f8f4b3a
Gavin OMelia <gavin@omelia.org>
2015-11-03 08:31:32 -0500

    BMI calculator

---
+++ lib/19_bmi_calculator/a.ex
@@ -0,0 +1,14 @@
+defmodule BMICalculator.A do
+  import InputRetriever
+  def go do
+    weight = retrieve("What is your weight? ")
+    height = retrieve("What is your height in inches? ")
+    bmi = (weight / (height * height)) * 703
+    IO.puts "Your BMI is #{bmi}"
+    if Enum.member?(18.5..25.0, bmi) do
+      IO.puts "You are within the ideal weight range."
+    else
+      IO.puts "You should see a doctor!"
+    end
+  end
+end
